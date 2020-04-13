1be754b44fe09559eed1b4f39ab093c700e8d545
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-04-13 20:58:00 +0700

    push bru

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 5000
 const cors = require('cors')
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
-app.use('/', router)
+app.use('/', router, function (req, res) {
+    res.send("Rest API ")
+})
 app.listen(port, function() {
     console.log(`Server running on http://localhost:${port}`)
 })
