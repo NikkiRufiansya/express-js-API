46b0ef8d2ae0eaac0b76b904454a733a975bd238
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-04-13 20:50:48 +0700

    a

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
-const router = require('./routes/routes')
+const router = require('./routes/router.js')
+const port = process.env.PORT || 5000
 const cors = require('cors')
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
-app.use('/', router, function (req, res) {
-    res.send("rest api by nikki")
-})
-app.listen(5000, function() {
-    console.log(`Server running on http://localhost:5000`)
+app.use('/', router)
+app.listen(port, function() {
+    console.log(`Server running on http://localhost:${port}`)
 })
 module.exports = app
